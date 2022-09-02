4cde4c889e358aa05a59e0890d638f8196c20e69
sai cherukuri <85233035+saicherukur@users.noreply.github.com>
2022-09-02 10:47:53 +0530

    def ispositive(n):     s=0     for i in range(1,n):
            if n % i == 0:
                
                s+=i
        if n==s:
            
            return True
        else:
            
            return False
    
    n=int(input())
    res=ispositive(n)
    print(res)

---
+++ Perfect_Number.py
@@ -0,0 +1,17 @@
+def ispositive(n):
+    s=0
+    for i in range(1,n):
+        
+        if n % i == 0:
+            
+            s+=i
+    if n==s:
+        
+        return True
+    else:
+        
+        return False
+
+n=int(input())
+res=ispositive(n)
+print(res)
